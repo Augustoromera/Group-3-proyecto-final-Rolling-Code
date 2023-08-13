34d12a2d2d17165c49dafac9ec4a6c1081af46d1
nicolaschia <nicolaschia9900@gmail.com>
2023-08-12 22:27:10 -0300

    Servicios que ofrece el local

--- src/home/components/BodyHome.jsx
+++ src/home/components/BodyHome.jsx
@@ -13,6 +13,27 @@ export const BodyHome = () => {
           
 	    </div>
 	</div>
+
+  <div className="container">
+	    <h3 className="text-center text-uppercase poppins-regular font-weight-bold">Nuestros servicios</h3>
+	    <br />
+	    <div className="row">
+	        <div className="col-12 col-sm-6 col-md-4">
+	            <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Envíos a domicilio</h5>
+	            <p className="text-center">Hacemos envíos dentro de las 4 avenidas, te enviamos tu pedido de forma más rápida del mercado gracias a nuestro equipo de cadetería.</p>
+	        </div>
+	        <div className="col-12 col-sm-6 col-md-4">
+	            <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Ventas al por mayor</h5>
+	            <p className="text-center">Querés deleitar a todos tus invitados en tus reuniones? No tan solo vendemos por menor, sino que tenemos los mejores precios por mayor para vos!</p>
+	        </div>
+	        <div className="col-12 col-sm-6 col-md-4">
+	            <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Reservaciones de local</h5>
+	            <p className="text-center">Hacemos reservas para todo tipos de eventos en nuestro local, que tu gente disfrute de las más auténticas y deliciosas hamburguesas. </p>
+	        </div>
+	    </div>
+	</div>
+
+	<hr />
     </>
   )
 }
