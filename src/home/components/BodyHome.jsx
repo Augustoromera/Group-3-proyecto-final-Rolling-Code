1ca80a431ad0987fbfe79281b28c6c82069b5517
nicolaschia <nicolaschia9900@gmail.com>
2023-08-12 21:50:53 -0300

    creando banner

--- src/home/components/BodyHome.jsx
+++ src/home/components/BodyHome.jsx
@@ -4,7 +4,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export const BodyHome = () => {
   return (
     <>
-
+      <div className="banner">
+	    <div className="banner-body">
+	        <h3 className="text-uppercase">Bienvenido a Rapiburgers</h3>
+	        <p>Las mejores hamburguesas y la mejor calidad los encuentras en Rapiburgers</p>
+	        <a href="menu.html" className="btn btn-warning"> &nbsp; Ir al menu</a>
+	    </div>
+	</div>
     </>
   )
 }
