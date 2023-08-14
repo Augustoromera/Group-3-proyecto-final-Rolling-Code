9c660dc6df2ef4b937362a9a6b853af23431e5e2
nicolaschia <nicolaschia9900@gmail.com>
2023-08-14 20:11:35 -0300

    Sección de servicios que ofrece la web, íconos con fontawesome y estilos agregados. Libreria react fontawesome agregada

--- src/home/components/BodyHome.jsx
+++ src/home/components/BodyHome.jsx
@@ -1,39 +1,48 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles/bodyHome.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart, faStoreSlash, faTruckFast } from '@fortawesome/free-solid-svg-icons';
 
 export const BodyHome = () => {
   return (
     <>
       <div className="banner">
-	    <div className="banner-body">
-	        <h3 className="text-uppercase">Bienvenido a Rapiburgers</h3>
-	        <p>Las mejores hamburguesas y la mejor calidad los encuentras en Rapiburgers</p>
-          
-	    </div>
-	</div>
+        <div className="banner-body">
+          <h3 className="text-uppercase">Bienvenido a Rapiburgers</h3>
+          <p>Las mejores hamburguesas y la mejor calidad los encuentras en Rapiburgers</p>
+        </div>
+      </div>
 
-  <div className="container">
-	    <h3 className="text-center text-uppercase poppins-regular font-weight-bold">Nuestros servicios</h3>
-	    <br />
-	    <div className="row">
-	        <div className="col-12 col-sm-6 col-md-4">
-	            <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Envíos a domicilio</h5>
-	            <p className="text-center">Hacemos envíos dentro de las 4 avenidas, te enviamos tu pedido de forma más rápida del mercado gracias a nuestro equipo de cadetería.</p>
-	        </div>
-	        <div className="col-12 col-sm-6 col-md-4">
-	            <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Ventas al por mayor</h5>
-	            <p className="text-center">Querés deleitar a todos tus invitados en tus reuniones? No tan solo vendemos por menor, sino que tenemos los mejores precios por mayor para vos!</p>
-	        </div>
-	        <div className="col-12 col-sm-6 col-md-4">
-	            <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Reservaciones de local</h5>
-	            <p className="text-center">Hacemos reservas para todo tipos de eventos en nuestro local, que tu gente disfrute de las más auténticas y deliciosas hamburguesas. </p>
-	        </div>
-	    </div>
-	</div>
+      <div className="container">
+        <h3 className="text-center text-uppercase poppins-regular font-weight-bold">Nuestros servicios</h3>
+        <br />
+        <div className="row">
+          <div className="col-12 col-sm-6 col-md-4">
+            <div className="service-item">
+              <FontAwesomeIcon className='service-icon' icon={faTruckFast} />
+              <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Envíos a domicilio</h5>
+              <p className="text-center">Hacemos envíos dentro de las 4 avenidas, te enviamos tu pedido de forma más rápida del mercado gracias a nuestro equipo de cadetería.</p>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4">
+            <div className="service-item">
+              <FontAwesomeIcon className='service-icon' icon={faShoppingCart} />
+              <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Ventas al por mayor</h5>
+              <p className="text-center">Querés deleitar a todos tus invitados en tus reuniones? No tan solo vendemos por menor, sino que tenemos los mejores precios por mayor para vos!</p>
+            </div>
+          </div>
+          <div className="col-12 col-sm-6 col-md-4">
+            <div className="service-item">
+              <FontAwesomeIcon className='service-icon' icon={faStoreSlash} />
+              <h5 className="text-center text-uppercase poppins-regular font-weight-bold">Reservaciones de local</h5>
+              <p className="text-center">Hacemos reservas para todo tipos de eventos en nuestro local, que tu gente disfrute de las más auténticas y deliciosas hamburguesas. </p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-	<hr />
+      <hr />
     </>
   )
 }
