acf338bd4a0de5ea61ce54ed5a4243c12f474d14
Paulo Srur <paulopepesrur@gmail.com>
2023-08-14 20:11:56 -0300

    agregando cards de cada uno con info

--- vite-project/src/home/pages/AboutUs.jsx
+++ vite-project/src/home/pages/AboutUs.jsx
@@ -8,11 +8,122 @@ export const AboutUs = () => {
     <>
     <Navbar/>
      <h1>Nuestro equipo</h1>
-     
+
      <div class="container-cuadrado">
         <div class="cuadrado cuadrado1"></div>
         <div class="cuadrado cuadrado2"></div>
         <div class="cuadrado cuadrado3"></div>
+    </div>
+    <div class="container-cards">
+    <div class="card">
+            <div class="cover-card">
+                <img src="/img/matias.jpg" alt="imagen de Matias, integrante del grupo"/>
+            </div>
+            <h2>Paulo Srur</h2>
+            <p class="p-espacio1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi repudiandae molestiae officia veniam! Fugiat magni sint molestias doloribus id enim aperiam. Debitis recusandae, nihil asperiores maiores accusantium culpa vero ipsum.</p>
+
+            <div class="wrapper">
+                <ul class="iconos">
+                    <li class="facebook"><a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                    <li class="instagram"><a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> </li>
+                    <li class="linkedin"><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a> </li>
+                </ul>
+               </div>
+
+            <hr/>
+            <div class="footer-card">
+                <h3 class="user-name"> Rolling Code</h3>
+                <i>45i</i>
+            </div>
+         </div>
+        
+         <div class="card">
+            <div class="cover-card">
+                <img src="/img/matias.jpg" alt="imagen de Matias, integrante del grupo"/>
+            </div>
+            <h2>Augusto Romera</h2>
+            <p class="p-espacio1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, magnam culpa quia accusamus omnis asperiores doloribus. Quo, voluptate voluptatibus. Numquam blanditiis vitae voluptate cumque natus sapiente tenetur libero, facere excepturi!</p>
+
+            <div class="wrapper">
+                <ul class="iconos">
+                    <li class="facebook"><a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                    <li class="instagram"><a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> </li>
+                    <li class="linkedin"><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a> </li>
+                </ul>
+               </div>
+
+            <hr/>
+            <div class="footer-card">
+                <h3 class="user-name"> Rolling Code</h3>
+                <i>45i</i>
+            </div>
+         </div>
+
+         <div class="card">
+            <div class="cover-card">
+                <img src="/img/damian.jpeg" alt="imagen de Damian, integrante del grupo"/>
+            </div>
+            <h2>Nicolas Chia</h2>
+            <p class="p-espacio2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, quia rem voluptatem nostrum optio nam debitis repudiandae perspiciatis quos eligendi temporibus aspernatur enim laudantium corrupti? Iure odit repellat sunt excepturi?</p>
+
+            <div class="wrapper">
+                <ul class="iconos">
+                    <li class="facebook"><a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                    <li class="instagram"><a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> </li>
+                    <li class="linkedin"><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a> </li>
+                </ul>
+               </div>
+
+            <hr/>
+            <div class="footer-card">
+                <h3 class="user-name"> Rolling Code</h3>
+                <i>45i</i>
+            </div>
+         </div>
+
+         <div class="card">
+            <div class="cover-card">
+                <img src="/img/ro.jpg" alt="imagen de Rocio, integrante del grupo."/>
+            </div>
+            <h2>Belen Cordoba</h2>
+            <p class="p-espacio3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, culpa. Rerum, repellat quos. Et esse iusto reprehenderit assumenda voluptas. Tempore, ab. Fuga reprehenderit quam voluptate ipsam consequatur officia modi obcaecati.</p>
+                   
+            <div class="wrapper">
+                <ul class="iconos">
+                    <li class="facebook"><a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                    <li class="instagram"><a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> </li>
+                    <li class="linkedin"><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a> </li>
+                </ul>
+               </div>
+
+            <hr/>
+            <div class="footer-card">
+                <h3 class="user-name"> Rolling Code</h3>
+                <i>45i</i>
+            </div>
+         </div>
+
+         <div class="card">
+            <div class="cover-card">
+                <img src="/img/yo (3).jpeg" alt="imagen de Paulo, integrante del grupo."/>
+            </div>
+            <h2>Santiago Gonzalez</h2>
+            <p class="p-espacio4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto officiis sint, in est laborum odit repellendus quis laudantium rerum quas atque perferendis commodi qui provident, asperiores facere consectetur illum. Saepe! </p>
+
+           <div class="wrapper">
+            <ul class="iconos">
+                <li class="facebook"><a href="https://www.facebook.com" target="_blank"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
+                <li class="instagram"><a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a> </li>
+                <li class="linkedin"><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a> </li>
+            </ul>
+           </div>
+
+            <hr/>
+            <div class="footer-card">
+                <h3 class="user-name"> Rolling Code</h3>
+                <i>45i</i>
+            </div>
+         </div>
     </div>
     
 
