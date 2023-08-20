47c0791ea21b114d7a12ac063452cebb41535260
nicolaschia <nicolaschia9900@gmail.com>
2023-08-20 03:28:51 -0300

    código más ordenado- tamaño de imagen de las card, posición de los precios arreglados

--- src/home/components/BodyHome.jsx
+++ src/home/components/BodyHome.jsx
@@ -26,6 +26,8 @@ export const BodyHome =  () => {
 
   return (
     <>
+
+    {/* ----------------banner---------------- */}
       <div className="banner">
         <div className="banner-body">
           <h3 className="text-uppercase">Bienvenido a Rapiburgers</h3>
@@ -33,13 +35,14 @@ export const BodyHome =  () => {
         </div>
       </div>
         <hr />
+
+    {/* ----------------sobre nosotros---------------- */}
         <div className="container-fluid">
           <div className="row">
-            <div className="col-6">
+            <div className="col-12 col-lg-6">
                 <img src="src/assets/images/nosotros/hamburguesa-nosotros-2.jpg" className='img-fluid rounded shadow-sm h-100 w-100 ' alt="imagen de hamburguesa" />
             </div>
-            <div className="col-6 d-flex flex-column justify-content-center align-items-start ">
-                
+            <div className="col-12 col-lg-6 d-flex flex-column justify-content-center align-items-start ">  
                 <div className='d-flex flex-column align-items-start' >
                 <h1 className='display-2 fw-bold d-flex flex-column align-items-center'>Sobre Nosotros</h1>
                 <h1 className='text-center display-5'>Rapiburgers</h1>
@@ -50,8 +53,9 @@ export const BodyHome =  () => {
             </div>
           </div>
         </div>
-
           <hr />
+
+    {/* ----------------servicios---------------- */}    
       <div className="container">
         <h3 className="text-center text-uppercase poppins-regular font-weight-bold">Nuestros servicios</h3>
         <br />
@@ -79,28 +83,34 @@ export const BodyHome =  () => {
           </div>
         </div>
       </div>
-
       <hr />
 
+    {/* ----------------menús---------------- */}
 	  <div className="container">
-	  <h3 className="text-center text-uppercase poppins-regular font-weight-bold">Menú</h3>
+	  <h3 className="text-center text-uppercase poppins-regular font-weight-bold">Nuestros menús más pedidos</h3>
 	  <br />
-	  </div>
-
     <div className="row">
 				{cargarProducto.map((menu) => (
-					<div key={menu._id} className="col-md-4 mb-4">
-						<Card>
-            <img src={menu.imagen} alt={menu.nombre} className="card-img-top" />
-							<Card.Body>
+					<div key={menu._id} className="col-md-4 col-lg-3 mb-4">
+						<Card className='h-100'>
+            <img src={menu.imagen} alt={menu.nombre} className="card-img-top img-fluid img-card-home" />
+							<Card.Body className='d-flex flex-column'>
 								<Card.Title>{menu.nombre}</Card.Title>
-								<Card.Text> $ {menu.precio}</Card.Text>
+								<div className="mt-auto">
+            <Card.Text > $ {menu.precio}</Card.Text>
+          </div>
 							</Card.Body>
 						</Card>
 					</div>
 				))}
 			</div>
-
+      </div>
+            <hr />
+      {/* ----------------mapa---------------- */}
+      <div className='text-center mb-4'>
+            <h2 className='fw-bold'>Dónde estamos?</h2>
+            <iframe className='w-75 ' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14240.408879082126!2d-65.2072018!3d-26.8367009!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94225d3ad7f30f1d%3A0xf8606cd659b8e3e4!2sRollingCode%20School!5e0!3m2!1ses-419!2sar!4v1690391123408!5m2!1ses-419!2sar" width="1000" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
     </>
   )
 }
