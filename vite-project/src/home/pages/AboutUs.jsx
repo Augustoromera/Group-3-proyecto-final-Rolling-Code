f69f2ab17280daa8737ae6b88aed4a79b1f8ff6e
Paulo Srur <paulopepesrur@gmail.com>
2023-08-21 12:46:09 -0300

    eliminacion de una card y ultimos cambios

--- vite-project/src/home/pages/AboutUs.jsx
+++ vite-project/src/home/pages/AboutUs.jsx
@@ -33,7 +33,7 @@ export const AboutUs = () => {
                 <img src={imgPaulo} alt="Paulo, integrante del grupo"/>
             </div>
             <h2>Paulo Srur</h2>
-            <p className="p-espacio1">Paulo Srur, 19 años, soy un apasionado de la tecnología y la programación desde chico, nunca dejaré de aprender y uno de mis objetivos es convertir esta pasión en una carrera sólida. Full stack MERN en progreso. Se inglés y un poco de portugués. Mi hobbie es el fútbol y aguante Messi. </p>
+            <p className="p-espacio1">Tengo 19 años, soy estudiante de licenciatura en informática en la UNT, un apasionado de la tecnología y la programación desde chico, nunca dejaré de aprender y uno de mis objetivos es convertir esta pasión en una carrera sólida. Full stack MERN en progreso. Se inglés y un poco de portugués. Mi hobbie es el fútbol y aguante Messi. </p>
 
             <div className="wrapper">
                 <ul className="iconos">
@@ -55,7 +55,7 @@ export const AboutUs = () => {
                 <img src={imgAugusto} alt="Augusto, integrante del grupo"/>
             </div>
             <h2>Augusto Romera</h2>
-            <p className="p-espacio2">Augusto Romera, 24 años, soy estudiante de ingeniería en sistemas de información, con estudios en rolling code para full stack developer disfruto explorando la naturaleza y los momentos al aire libre. Además, encuentro fascinación en la lógica y en enfrentar desafíos.</p>
+            <p className="p-espacio2">Tengo 24 años, soy estudiante de ingeniería en sistemas de información, con estudios en rolling code para full stack developer disfruto explorando la naturaleza y los momentos al aire libre. Además, encuentro fascinación en la lógica y en enfrentar desafíos.</p>
 
             <div className="wrapper">
                 <ul className="iconos">
@@ -77,7 +77,7 @@ export const AboutUs = () => {
                 <img src={imgNico} alt="Nicolas, integrante del grupo"/>
             </div>
             <h2>Nicolas Chia</h2>
-            <p className="p-espacio3">Nicolas Chia, 28 años, soy Técnico Mecatrónico y estudio Ing Civil en la UTN. También estoy desarrollando habilidades para ser un Full Stack Developer en Rolling Code. Soy apasionado por el fútbol y compartir con amigos.</p>
+            <p className="p-espacio3">Tengo 28 años, soy Técnico Mecatrónico y estudio Ing Civil en la UTN. También estoy desarrollando habilidades para ser un Full Stack Developer en Rolling Code. Soy apasionado por el fútbol y compartir con amigos.</p>
 
             <div className="wrapper">
                 <ul className="iconos">
@@ -94,34 +94,13 @@ export const AboutUs = () => {
             </div>
          </div>
 
-         <div className="card">
-            <div className="cover-card">
-                <img src={imgBelen} alt="Belen, integrante del grupo."/>
-            </div>
-            <h2>Belen Cordoba</h2>
-            <p className="p-espacio4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, culpa. Rerum, repellat quos. Et esse iusto reprehenderit assumenda voluptas. Tempore, ab. Fuga reprehenderit quam voluptate ipsam consequatur officia modi obcaecati.</p>
-                   
-            <div className="wrapper">
-                <ul className="iconos">
-                    <li className="github"><a href="https://www.github.com" target="_blank"><i className="fa fa-github fa-2x" aria-hidden="true"></i></a></li>
-                    <li className="instagram"><a href="https://www.instagram.com" target="_blank"><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></a> </li>
-                    <li className="linkedin"><a href="https://www.linkedin.com" target="_blank"><i className="fa fa-linkedin fa-2x" aria-hidden="true"></i></a> </li>
-                </ul>
-               </div>
-
-            <hr/>
-            <div className="footer-card">
-                <h3 className="user-name"> Rolling Code</h3>
-                <i>45i</i>
-            </div>
-         </div>
 
          <div className="card">
             <div className="cover-card">
                 <img src={imgSantiago} alt="Santiago, integrante del grupo."/>
             </div>
             <h2>Santiago Gonzalez</h2>
-            <p className="p-espacio5">Santiago Gonzalez, 18 años, soy estudiante del Colegio Lorenzo Massa, programador Full stack, con un nivel medio-avanzado de ingles. En mi tiempo libre me gusta ver series y ir al gimnasio. y Espero poder seguir desarrollando mis habilidades como programador constantemente. </p>
+            <p className="p-espacio5">Tengo 18 años, soy estudiante del Colegio Lorenzo Massa, programador Full stack, con un nivel medio-avanzado de ingles. En mi tiempo libre me gusta ver series y ir al gimnasio. y Espero poder seguir desarrollando mis habilidades como programador constantemente. </p>
 
            <div className="wrapper">
             <ul className="iconos">
@@ -145,4 +124,4 @@ export const AboutUs = () => {
 
     </>
   )
-}
+};
