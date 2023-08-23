27f1597b15d7c147c6dcb9f0f578d609488dbe85
nicolaschia <nicolaschia9900@gmail.com>
2023-08-23 03:52:17 -0300

    rutas modificadas con la nueva estructuracion de carpetas

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,8 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { HomeScreen } from '../home/pages/HomeScreen'
-import { ContactScreen } from '../contact/page/ContactScreen'
+import { HomeScreen } from '../pages/HomeScreen'
+import { ContactScreen } from '../pages/contact/ContactScreen'
 
 export const AppRouter = () => {
     return (
