b21684aa9c74970d7d448fb559cdc2032e714d9a
Augusto <newsromers@gmail.com>
2023-09-07 17:11:12 -0300

    Se arreglo el inicio de de sesion, reparando/modificando el componente Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,29 @@ import logoTipo from '../assets/images/logo/logotipo.png'
 
 function Header() {
   const { isAuthenticated, logout, user } = useAuth();
-
+  if (user === null) {
+    return (
+      <Navbar expand="lg" className="bg-body-tertiary navbarmain" bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            <img src={logoTipo} className="d-inline-block icon-burger" alt="Logo" />
+            Rapiburgers
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="/aboutus" className="nav-link">Sobre Nosotross</Nav.Link>
+              <Nav.Link as={Link} to="/contact" className="nav-link">Contacto</Nav.Link>
+              <NavDropdown title="Ingresar" id="basic-nav-dropdown">
+                <NavDropdown.Item as={Link} to="/login" className="nav-link">Iniciar Sesión</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/register" className="nav-link">Registrarse</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
   return (
     <Navbar expand="lg" className="bg-body-tertiary navbarmain" bg="dark" data-bs-theme="dark">
       <Container>
@@ -37,7 +59,7 @@ function Header() {
               </>
             )}
             {/* Mostrar elementos de usuario normal */}
-            {isAuthenticated ? (
+            {(isAuthenticated && !(user.role === 'admin')) ? (
               <>
                 <Nav.Link as={Link} to="/pedidos" className='nav-link'>Pedidos</Nav.Link>
                 <Nav.Link as={Link} to="/aboutus" className="nav-link">Sobre Nosotros</Nav.Link>
@@ -46,17 +68,7 @@ function Header() {
                   logout();
                 }}>Logout</Nav.Link>
               </>
-            ) : (
-              <>
-                <Nav.Link as={Link} to="/aboutus" className="nav-link">Sobre Nosotros</Nav.Link>
-                <Nav.Link as={Link} to="/contact" className="nav-link">Contacto</Nav.Link>
-                <NavDropdown title="Ingresar" id="basic-nav-dropdown">
-                  <NavDropdown.Item as={Link} to="/login" className="nav-link">Iniciar Sesión</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to="/register" className="nav-link">Registrarse</NavDropdown.Item>
-                </NavDropdown>
-
-              </>
-            )}
+            ) : ("")}
 
           </Nav>
         </Navbar.Collapse>
