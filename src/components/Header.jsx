f988a4376c361a36b845af6b762e08de68fb56f4
Augusto <newsromers@gmail.com>
2023-09-16 00:15:13 -0300

    Cambios en el navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,19 @@
-
-import React from 'react';
 import { NavDropdown, Nav } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import "../pages/styles/header.css";
-import logoTipo from '../assets/images/logo/logotipo.png'
+import logoTipo from '../assets/images/logo/logorapiburgeramarillo.png'
 import { useNavigate } from 'react-router-dom';
 import swal from 'sweetalert2';
 
-function Header() {
+// eslint-disable-next-line react/prop-types
+function Header({ navBarClass }) {
   const { isAuthenticated, logout, user } = useAuth();
   const navigate = useNavigate();
+  const navClass = location.pathname === '/' ? 'navbarhome' : 'navbarmain';
+
   const handleLogout = () => {
     swal.fire({
       title: "Cerrando sesión...",
@@ -25,25 +26,32 @@ function Header() {
       navigate("/"); // Navega al home ("/") después de cerrar sesión
     });
   };
+  const navLinkClass = navBarClass;
+  let navLinkClassWelcome;
+  if (navLinkClass === "none-class") {
+    navLinkClassWelcome = "inline-block-class";
+  } else {
+      navLinkClassWelcome = "none-class";
+  }
+  console.log(navLinkClassWelcome)
 
   if (user === null) {
     return (
-      <Navbar expand="lg" data-bs-theme="dark" className=" navbarmain" style={{ backgroundColor: 'black' }}>
+      <Navbar expand="lg" data-bs-theme="dark" className={navClass} >
 
         <Container>
           <Navbar.Brand as={Link} to="/">
-            <img src={logoTipo} className="d-inline-block icon-burger navbar-image me-3" alt="Logo" />
-            <span className="brand-text">Rapiburgers</span>
+            <img src={logoTipo} className="d-inline-block navbar-image " alt="Logo" />
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/aboutus" className="nav-link">Sobre Nosotros</Nav.Link>
-              <Nav.Link as={Link} to="/contact" className="nav-link">Contacto</Nav.Link>
-              <NavDropdown title="Ingresar" id="basic-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/login" className="nav-link">Iniciar Sesión</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/register" className="nav-link">Registrarse</NavDropdown.Item>
+              <Nav.Link as={Link} to="/aboutus" className={navLinkClass}>Sobre Nosotros</Nav.Link>
+              <Nav.Link as={Link} to="/contact" className={navLinkClass}>Contacto</Nav.Link>
+              <NavDropdown title="Ingresar" id="basic-nav-dropdown" className={navLinkClass}>
+                <NavDropdown.Item as={Link} to="/login" >Iniciar Sesión</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/register" className={navLinkClass}>Registrarse</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
@@ -52,11 +60,10 @@ function Header() {
     );
   }
   return (
-    <Navbar expand="lg" data-bs-theme="dark" className=" navbarmain" style={{ backgroundColor: 'black' }}>
+    <Navbar expand="lg" data-bs-theme="dark" className={navClass} style={{ backgroundColor: 'transparent' }}>
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <img src={logoTipo} className="d-inline-block icon-burger" alt="Logo" />
-          <span className="brand-text">Rapiburgers</span>
+          <img src={logoTipo} className="d-inline-block navbar-image " alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
@@ -65,24 +72,24 @@ function Header() {
             {/*Mostrar enlace de administrador solo si el usuario es un administrador */}
             {isAuthenticated && user.role === 'admin' && (
               <>
-                <Nav.Link as={Link} to="/" className='nav-welcome'>Bienvenid@ {user.username}!</Nav.Link>
-                <Nav.Link as={Link} to="/admin" className='nav-link'>
+                <Nav.Link as={Link} to="/" className={`nav-welcome ${navLinkClassWelcome}`}>Bienvenid@ {user.username}!</Nav.Link>
+                <Nav.Link as={Link} to="/admin" className={navLinkClass}>
                   Administración
                 </Nav.Link>
-                <Nav.Link as={Link} to="/pedidos" className='nav-link'>Pedidos</Nav.Link>
-                <Nav.Link as={Link} to="/aboutus" className="nav-link">Sobre Nosotros</Nav.Link>
-                <Nav.Link as={Link} to="/contact" className="nav-link">Contacto</Nav.Link>
-                <Nav.Link as={Link} className='nav-link' to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
+                <Nav.Link as={Link} to="/pedidos" className={navLinkClass}>Pedidos</Nav.Link>
+                <Nav.Link as={Link} to="/aboutus" className={navLinkClass}>Sobre Nosotros</Nav.Link>
+                <Nav.Link as={Link} to="/contact" className={navLinkClass}>Contacto</Nav.Link>
+                <Nav.Link as={Link} className={navLinkClass} to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
               </>
             )}
             {/* Mostrar elementos de usuario normal */}
             {(isAuthenticated && !(user.role === 'admin')) ? (
               <>
-                <Nav.Link as={Link} to="/" className='nav-welcome'>Bienvenid@ {user.username}!</Nav.Link>
-                <Nav.Link as={Link} to="/pedidos" className='nav-link'>Pedidos</Nav.Link>
-                <Nav.Link as={Link} to="/aboutus" className="nav-link">Sobre Nosotros</Nav.Link>
-                <Nav.Link as={Link} to="/contact" className="nav-link">Contacto</Nav.Link>
-                <Nav.Link as={Link} className='nav-link' to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
+                <Nav.Link as={Link} to="/" className={`nav-welcome ${navLinkClass}`}>Bienvenid@ {user.username}!</Nav.Link>
+                <Nav.Link as={Link} to="/pedidos" className={navLinkClass}>Pedidos</Nav.Link>
+                <Nav.Link as={Link} to="/aboutus" className={navLinkClass}>Sobre Nosotros</Nav.Link>
+                <Nav.Link as={Link} to="/contact" className={navLinkClass}>Contacto</Nav.Link>
+                <Nav.Link as={Link} className={navLinkClass} to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
               </>
             ) : ("")}
 
--- src/pages/HomeScreen.jsx
+++ src/pages/HomeScreen.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header';
 import BodyHome from '../components/BodyHome';
 import { Footer } from '../components/Footer';
 
 export const HomeScreen = () => {
+    const [navBarClass, setNavBarClass] = useState('');
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+            const windowHeight = window.innerHeight;
+            const scrollThreshold = 0.3;
+
+            if (scrollY > windowHeight * scrollThreshold) {
+                setNavBarClass("none-class");
+            } else {
+                setNavBarClass("inline-block-class");
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
         <div>
-            <Header />
+            <Header navBarClass={navBarClass} />
             <BodyHome />
             <Footer />
         </div>
