2d58d87cf6f93fffc60f780b99c8feb243b04b91
Paulo Srur <paulopepesrur@gmail.com>
2023-09-17 17:53:56 -0300

    mensaje de welcome solucionado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,13 @@ function Header({ navBarClass }) {
       navigate("/"); // Navega al home ("/") después de cerrar sesión
     });
   };
- 
+  const navLinkClass = navBarClass;
+  let navLinkClassWelcome;
+  if (navLinkClass === "none-class") {
+    navLinkClassWelcome = "inline-block-class";
+  } else {
+      navLinkClassWelcome = "none-class";
+  }
   if (user === null) {
     return (
       <Navbar expand="lg" data-bs-theme="dark" className={navClass} >
@@ -39,11 +45,11 @@ function Header({ navBarClass }) {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link as={Link} to="/aboutus" >Sobre Nosotros</Nav.Link>
-              <Nav.Link as={Link} to="/contact" >Contacto</Nav.Link>
-              <NavDropdown title="Ingresar" id="basic-nav-dropdown" >
+              <Nav.Link as={Link} to="/aboutus" className={navLinkClass}>Sobre Nosotros</Nav.Link>
+              <Nav.Link as={Link} to="/contact" className={navLinkClass}>Contacto</Nav.Link>
+              <NavDropdown title="Ingresar" id="basic-nav-dropdown" className={navLinkClass}>
                 <NavDropdown.Item as={Link} to="/login" >Iniciar Sesión</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/register" >Registrarse</NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/register" className={navLinkClass}>Registrarse</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
@@ -65,23 +71,23 @@ function Header({ navBarClass }) {
             {isAuthenticated && user.role === 'admin' && (
               <>
                 <Nav.Link as={Link} to="/" className={`nav-welcome ${navLinkClassWelcome}`}>Bienvenid@ {user.username}!</Nav.Link>
-                <Nav.Link as={Link} to="/admin" >
+                <Nav.Link as={Link} to="/admin" className={navLinkClass}>
                   Administración
                 </Nav.Link>
-                <Nav.Link as={Link} to="/pedidos" >Pedidos</Nav.Link>
-                <Nav.Link as={Link} to="/aboutus" >Sobre Nosotros</Nav.Link>
-                <Nav.Link as={Link} to="/contact" >Contacto</Nav.Link>
-                <Nav.Link as={Link}  to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
+                <Nav.Link as={Link} to="/pedidos" className={navLinkClass}>Pedidos</Nav.Link>
+                <Nav.Link as={Link} to="/aboutus" className={navLinkClass}>Sobre Nosotros</Nav.Link>
+                <Nav.Link as={Link} to="/contact" className={navLinkClass}>Contacto</Nav.Link>
+                <Nav.Link as={Link} className={navLinkClass} to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
               </>
             )}
             {/* Mostrar elementos de usuario normal */}
             {(isAuthenticated && !(user.role === 'admin')) ? (
               <>
                 <Nav.Link as={Link} to="/" className={`nav-welcome ${navLinkClass}`}>Bienvenid@ {user.username}!</Nav.Link>
-                <Nav.Link as={Link} to="/pedidos" >Pedidos</Nav.Link>
-                <Nav.Link as={Link} to="/aboutus" >Sobre Nosotros</Nav.Link>
-                <Nav.Link as={Link} to="/contact" >Contacto</Nav.Link>
-                <Nav.Link as={Link}  to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
+                <Nav.Link as={Link} to="/pedidos" className={navLinkClass}>Pedidos</Nav.Link>
+                <Nav.Link as={Link} to="/aboutus" className={navLinkClass}>Sobre Nosotros</Nav.Link>
+                <Nav.Link as={Link} to="/contact" className={navLinkClass}>Contacto</Nav.Link>
+                <Nav.Link as={Link} className={navLinkClass} to="/" onClick={handleLogout}>Cerrar sesión</Nav.Link>
               </>
             ) : ("")}
 
