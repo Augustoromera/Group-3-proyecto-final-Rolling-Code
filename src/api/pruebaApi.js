933a348f4d9fb2a2f3caf6bda54638d5df3d4501
Augusto <newsromers@gmail.com>
2023-10-11 18:35:58 -0300

    Cambios en auth.js

--- src/api/pruebaApi.js
+++ src/api/pruebaApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const pruebaApi = axios.create({
-    baseURL: 'https://backendrapuburgers.up.railway.app',
+    baseURL: 'http://localhost:8080/',
 });
 
 export default pruebaApi;
