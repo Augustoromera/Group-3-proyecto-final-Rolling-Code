b459539329b9a8f0cb9a3f82e69ca08e2948581b
Paulo Srur <paulopepesrur@gmail.com>
2024-09-02 18:42:09 -0300

    cambio de imagen

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/about.css';
 import 'font-awesome/css/font-awesome.min.css';
-import ImagenPaulo from '../assets/images/profiles/yo.jpeg';
+import ImagenPaulo from '../assets/images/profiles/messias.jpg';
 import ImagenAugusto from '../assets/images/profiles/Augusto.jpeg';
 import ImagenNicolas from '../assets/images/profiles/Nico.jpeg';
 import ImagenSantiago from '../assets/images/profiles/Santiago.jpg';
